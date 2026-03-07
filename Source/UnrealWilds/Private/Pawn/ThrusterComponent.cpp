12cbcfe394815b842800827e77bbc15c5b818171
VA11-Hall-A <82312774+VA11-Hall-A@users.noreply.github.com>
2026-03-07 18:02:39 +0800

    Feat: 新增 ThrusterComponent 用于处理角色/载具推进逻辑

---
+++ Source/UnrealWilds/Private/Pawn/ThrusterComponent.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pawn/ThrusterComponent.h"
+
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// Sets default values for this component's properties
+UThrusterComponent::UThrusterComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+void UThrusterComponent::AddForceToMovemntComponent(FVector InputDirection)
+{
+	if (bIsCharacterMode)
+	{
+		MovementComponent->AddForce(InputDirection * ThrustForce);
+	}
+	else
+	{
+		PrimitiveComponent->AddForce(InputDirection*ThrustForce);
+	}
+}
+
+
+// Called when the game starts
+void UThrusterComponent::BeginPlay()
+{
+	Super::BeginPlay();
+ 
+	// Cache owner once for efficiency and readability
+	AActor* OwnerActor = GetOwner();
+	if (!OwnerActor)
+	{
+		// Owner is null, cannot proceed safely
+		UE_LOG(LogTemp, Warning, TEXT("%s: Owner actor is null in BeginPlay"), *GetName());
+		return;
+	}
+ 
+	// Attempt to cast owner to AUWCharacter to get movement component
+	if (ACharacter* Character = Cast<ACharacter>(OwnerActor))
+	{
+		MovementComponent = Character->GetCharacterMovement();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s: Owner is not an AUWCharacter"), *GetName());
+	}
+    
+	// Get root component and cast to UPrimitiveComponent for physics or rendering
+	PrimitiveComponent = Cast<UPrimitiveComponent>(OwnerActor->GetRootComponent());
+	if (!PrimitiveComponent)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s: Owner has no valid UPrimitiveComponent root"), *GetName());
+	}
+}
---
+++ Source/UnrealWilds/Public/Pawn/ThrusterComponent.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ThrusterComponent.generated.h"
+
+class UCharacterMovementComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class UNREALWILDS_API UThrusterComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UThrusterComponent();
+
+	void AddForceToMovemntComponent(FVector InputDirection);
+
+	UPROPERTY(EditAnywhere)
+	double ThrustForce = 0;
+	UPROPERTY(EditDefaultsOnly)
+	bool bIsCharacterMode = true;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UCharacterMovementComponent* MovementComponent = nullptr;
+	UPrimitiveComponent* PrimitiveComponent = nullptr;
+	
+};
